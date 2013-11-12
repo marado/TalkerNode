d02277113bb1483980539a191f33b3d587c0ef38
Marcos Marado <mindboosternoori@gmail.com>
2013-11-12 19:11:57 +0000

    .help

--- TalkerNode.js
+++ TalkerNode.js
@@ -94,6 +94,9 @@ function doCommand(socket, command) {
 		case ".who":
 			who(socket);
 			break;	
+		case ".help":
+			help(socket);
+			break;	
 		default:
 			socket.write("There's no such thing as a " + command.split(' ')[0] + " command.\r\n");
 			break;
@@ -166,6 +169,18 @@ function who(socket) {
 
 }
 
+function help(socket) {
+	socket.write("+----------------------------------------------------------------------------+\r\n");
+	socket.write("   Helpful commands on " + talkername + "\r\n");
+	socket.write("+----------------------------------------------------------------------------+\r\n");
+	socket.write("| .help - shows you this list of commands and what do they do                |\r\n");
+	socket.write("| .say  - lets you talk with other people. Just .say something!              |\r\n");
+	socket.write("| .quit - leaving us, are you? Then .quit !                                  |\r\n");
+	socket.write("| .who  - lets you know who is connected in the talker at this moment        |\r\n");
+	socket.write("+----------------------------------------------------------------------------+\r\n");
+	socket.write("| Remember: all commands start with a dot (.), like .help                    |\r\n");
+	socket.write("+----------------------------------------------------------------------------+\r\n");
+}
 
 /* 
  * AND FINALLY... THE ACTUAL main()!
