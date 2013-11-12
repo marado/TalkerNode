e3b4780a90f30c8cbf35030738767f817a7421f9
Marcos Marado <mindboosternoori@gmail.com>
2013-11-12 19:27:32 +0000

    version 0.0.1

--- TalkerNode.js
+++ TalkerNode.js
@@ -4,10 +4,9 @@
 var net = require('net');
 
 var sockets = [];
-
 var port = process.env.PORT || 8888;
-
 var talkername = "Moosville";
+var version = "0.0.1";
 
 /*
  * Cleans the input of carriage return, newline and control characters
@@ -52,7 +51,8 @@ function receiveData(socket, data) {
 
 	if(socket.username == undefined) {
 		if (cleanData.toLowerCase() === "quit") return socket.end('Goodbye!\n');
-		if (cleanData.toLowerCase() === "who") return who(socket);
+		if (cleanData.toLowerCase() === "who") { who(socket); return socket.write("Give me a name:  "); }
+		if (cleanData.toLowerCase() === "version") { show_version(socket); return socket.write("Give me a name:  "); }
 		var reservedNames=["who","quit","version"];
 		if (reservedNames.indexOf(cleanData.toLowerCase()) > -1) {
 			socket.write("\r\nThat username is reserved, you cannot have it.\r\nGive me a name:  ");
@@ -96,6 +96,9 @@ function doCommand(socket, command) {
 			break;	
 		case ".help":
 			help(socket);
+			break;	
+		case ".version":
+			show_version(socket);
 			break;	
 		default:
 			socket.write("There's no such thing as a " + command.split(' ')[0] + " command.\r\n");
@@ -173,13 +176,18 @@ function help(socket) {
 	socket.write("+----------------------------------------------------------------------------+\r\n");
 	socket.write("   Helpful commands on " + talkername + "\r\n");
 	socket.write("+----------------------------------------------------------------------------+\r\n");
-	socket.write("| .help - shows you this list of commands and what do they do                |\r\n");
-	socket.write("| .say  - lets you talk with other people. Just .say something!              |\r\n");
-	socket.write("| .quit - leaving us, are you? Then .quit !                                  |\r\n");
-	socket.write("| .who  - lets you know who is connected in the talker at this moment        |\r\n");
+	socket.write("| .help    - shows you this list of commands and what do they do             |\r\n");
+	socket.write("| .say     - lets you talk with other people. Just .say something!           |\r\n");
+	socket.write("| .quit    - leaving us, are you? Then .quit !                               |\r\n");
+	socket.write("| .who     - lets you know who is connected in the talker at this moment     |\r\n");
+	socket.write("| .version - gives you information regarding the software this talker runs   |\r\n");
 	socket.write("+----------------------------------------------------------------------------+\r\n");
 	socket.write("| Remember: all commands start with a dot (.), like .help                    |\r\n");
 	socket.write("+----------------------------------------------------------------------------+\r\n");
+}
+
+function show_version(socket) {
+	socket.write("TalkerNode, version " + version + "\r\n");
 }
 
 /* 
