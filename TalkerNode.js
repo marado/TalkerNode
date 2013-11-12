b6bff147846d7434ebb4da5eed62c3dbecf4920c
Marcos Marado <mindboosternoori@gmail.com>
2013-11-12 11:30:55 +0000

    indentation homogeneization

--- TalkerNode.js
+++ TalkerNode.js
@@ -13,8 +13,8 @@ var talkername = "Moosville";
  * Cleans the input of carriage return, newline and control characters
  */
 function cleanInput(data) {
-        var newString = data.toString().replace("[\u0000-\u001f]", "");
-        return newString.replace(/(\r\n|\n|\r)/gm,"");
+	var newString = data.toString().replace("[\u0000-\u001f]", "");
+	return newString.replace(/(\r\n|\n|\r)/gm,"");
 }
 
 /*
@@ -23,82 +23,80 @@ function cleanInput(data) {
 function receiveData(socket, data) {
 
 
-        var cleanData = cleanInput(data);
+	var cleanData = cleanInput(data);
+	
+	if(cleanData.length == 0)
+		return;
 
-        if(cleanData.length == 0)
-                return;
+	// Useful when in debug mode, you don't want this otherwise... it wouldn't be nice for your users' privacy, would it?
+	// console.log("Moo [" + cleanData + "]");
 
-		// Useful when in debug mode, you don't want this otherwise... it wouldn't be nice for your users' privacy, would it?
-        // console.log("Moo [" + cleanData + "]");
+	// TODO: We're just filtering out IAC commands. We should be dealing with them instead...
+	// IAC commands
+	var IAC = [
+		1 , // Telnet IAC - Echo
+		3 , // Telnet IAC - Suppress Go Ahead
+		5 , // Telnet IAC - Status
+		6 , // Telnet IAC - Timing Mark
+		24, // Telnet IAC - Terminal Type
+		31, // Telnet IAC - Window Size
+		32, // Telnet IAC - Window Speed
+		33, // Telnet IAC - Remote Flow Control
+		34, // Telnet IAC - Linemode
+		36  // Telnet IAC - Environment Variables
+	];
+	if (IAC.indexOf(cleanData.charCodeAt(0)) !== -1) {
+		// This is IAC, not an user input
+		console.log("Moo: IAC: [" + cleanData.charCodeAt(0) +"]");
+		return;
+	}
 
-		// TODO: We're just filtering out IAC commands. We should be dealing with them instead...
-		// IAC commands
-		var IAC = [
-			1 , // Telnet IAC - Echo
-			3 , // Telnet IAC - Suppress Go Ahead
-			5 , // Telnet IAC - Status                
-			6 , // Telnet IAC - Timing Mark
-			24, // Telnet IAC - Terminal Type
-			31, // Telnet IAC - Window Size
-			32, // Telnet IAC - Window Speed
-			33, // Telnet IAC - Remote Flow Control
-			34, // Telnet IAC - Linemode
-			36  // Telnet IAC - Environment Variables
-		];
-		if (IAC.indexOf(cleanData.charCodeAt(0)) !== -1) {
-			// This is IAC, not an user input
-			console.log("Moo: IAC: [" + cleanData.charCodeAt(0) +"]");
-		    return;
+	if(socket.username == undefined) {
+		// TODO: check if username is valid. We will probably only want a-Z characters, with a minimum and maximum length.
+		// TODO: check if the username isn't reserved. Names like who, quit and version are usually reserved.
+		// TODO: check if the username is already in use
+		socket.username = cleanData;
+		socket.write("\r\n\r\nWelcome " + socket.username + "\r\n");
+		// TODO: tell everyone else that you've connected
+		return;
+	}
+
+	// Chain of commands:
+	if(cleanData === ".quit") {
+		socket.end('Goodbye!\n');
+	} else {
+		for(var i = 0; i<sockets.length; i++) {
+			if (sockets[i] !== socket) {
+				if (typeof sockets[i].username != 'undefined') sockets[i].write(socket.username + ": " + data);
+			} else {
+				socket.write("You said: " + data);
+			}
 		}
-
-        if(socket.username == undefined)
-        {
-				// TODO: check if username is valid. We will probably only want a-Z characters, with a minimum and maximum length.
-				// TODO: check if the username isn't reserved. Names like who, quit and version are usually reserved.
-				// TODO: check if the username is already in use
-                socket.username = cleanData;
-                socket.write("\r\n\r\nWelcome " + socket.username + "\r\n");
-				// TODO: tell everyone else that you've connected
-				return;
-        }
-
-		// Chain of commands:
-        if(cleanData === ".quit") {
-                socket.end('Goodbye!\n');
-        }
-        else {
-                for(var i = 0; i<sockets.length; i++) {
-                        if (sockets[i] !== socket) {
-								if (typeof sockets[i].username != 'undefined') sockets[i].write(socket.username + ": " + data);
-                        } else {
-								socket.write("You said: " + data);
-						}
-                }
-        }
+	}
 }
 
 /*
  * Method executed when a socket ends
  */
 function closeSocket(socket) {
-        var i = sockets.indexOf(socket);
-        if (i != -1) {
-                sockets.splice(i, 1);
-        }
+	var i = sockets.indexOf(socket);
+	if (i != -1) {
+		sockets.splice(i, 1);
+	}
 }
 
 /*
  * Callback method executed when a new TCP socket is opened.
  */
 function newSocket(socket) {
-        sockets.push(socket);
-        socket.write('Welcome to the '+talkername+'!\r\n\r\nGive me a name:  ');
-        socket.on('data', function(data) {
-                receiveData(socket, data);
-        })
-        socket.on('end', function() {
-                closeSocket(socket);
-        })
+	sockets.push(socket);
+	socket.write('Welcome to the '+talkername+'!\r\n\r\nGive me a name:  ');
+	socket.on('data', function(data) {
+		receiveData(socket, data);
+	})
+	socket.on('end', function() {
+		closeSocket(socket);
+	})
 }
 
 // Create a new server and provide a callback for when a connection occurs
