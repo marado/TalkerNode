9db9c429eb6d4c666b3a70616a7692b2cb85cb98
Marcos Marado <mindboosternoori@gmail.com>
2013-12-20 19:02:02 +0000

    .password wasn't showing on .help

--- TalkerNode.js
+++ TalkerNode.js
@@ -265,19 +265,20 @@ function who(socket) {
 }
 
 function help(socket) {
-	socket.write("+----------------------------------------------------------------------------+\r\n");
+	socket.write("+-----------------------------------------------------------------------------+\r\n");
 	socket.write("   Helpful commands on " + talkername + "\r\n");
-	socket.write("+----------------------------------------------------------------------------+\r\n");
-	socket.write("| .help    - shows you this list of commands and what do they do             |\r\n");
-	socket.write("| .say     - lets you talk with other people. Just .say something!           |\r\n");
-	socket.write("| .emote   - lets you pose something, as if you were acting.                 |\r\n");
-	socket.write("| .quit    - leaving us, are you? Then .quit !                               |\r\n");
-	socket.write("| .who     - lets you know who is connected in the talker at this moment     |\r\n");
-	socket.write("| .version - gives you information regarding the software this talker runs   |\r\n");
-	socket.write("| .tell    - tells someone something, in private. Only both of you will know |\r\n");
-	socket.write("+----------------------------------------------------------------------------+\r\n");
-	socket.write("| Remember: all commands start with a dot (.), like .help                    |\r\n");
-	socket.write("+----------------------------------------------------------------------------+\r\n");
+	socket.write("+-----------------------------------------------------------------------------+\r\n");
+	socket.write("| .help     - shows you this list of commands and what do they do             |\r\n");
+	socket.write("| .say      - lets you talk with other people. Just .say something!           |\r\n");
+	socket.write("| .emote    - lets you pose something, as if you were acting.                 |\r\n");
+	socket.write("| .quit     - leaving us, are you? Then .quit !                               |\r\n");
+	socket.write("| .who      - lets you know who is connected in the talker at this moment     |\r\n");
+	socket.write("| .version  - gives you information regarding the software this talker runs   |\r\n");
+	socket.write("| .tell     - tells someone something, in private. Only both of you will know |\r\n");
+	socket.write("| .password - use this if you want to change your password                    |\r\n");
+	socket.write("+-----------------------------------------------------------------------------+\r\n");
+	socket.write("| Remember: all commands start with a dot (.), like .help                     |\r\n");
+	socket.write("+-----------------------------------------------------------------------------+\r\n");
 }
 
 function show_version(socket) {
