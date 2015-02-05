b16002e1e63914723fa2d5d5a6f843cfcc3c0c66
Marcos Marado <mindboosternoori@gmail.com>
2015-02-05 18:29:03 +0000

    0.1.5: .ranks on .help. Ref: #3

--- TalkerNode.js
+++ TalkerNode.js
@@ -7,7 +7,7 @@ var crypto = require('crypto');
 var sockets = [];
 var port = process.env.PORT || 8888; // TODO: move to talker settings database
 var talkername = "Moosville";        // TODO: move to the talker settings database
-var version = "0.1.4";
+var version = "0.1.5";
 
 // Instantiates the users database
 var dirty = require('dirty');
@@ -364,11 +364,12 @@ function help(socket) {
 	socket.write("| .help     - shows you this list of commands and what do they do             |\r\n");
 	socket.write("| .say      - lets you talk with other people. Just .say something!           |\r\n");
 	socket.write("| .emote    - lets you pose something, as if you were acting.                 |\r\n");
-	socket.write("| .quit     - leaving us, are you? Then .quit !                               |\r\n");
-	socket.write("| .who      - lets you know who is connected in the talker at this moment     |\r\n");
-	socket.write("| .version  - gives you information regarding the software this talker runs   |\r\n");
 	socket.write("| .tell     - tells someone something, in private. Only both of you will know |\r\n");
+	socket.write("| .who      - lets you know who is connected in the talker at this moment     |\r\n");
+	socket.write("| .ranks    - list the talker's ranks, and points out which one is yourse     |\r\n");
+	socket.write("| .version  - gives you information regarding the software this talker runs   |\r\n");
 	socket.write("| .password - use this if you want to change your password                    |\r\n");
+	socket.write("| .quit     - leaving us, are you? Then .quit !                               |\r\n");
 	socket.write("+-----------------------------------------------------------------------------+\r\n");
 	socket.write("| Remember: all commands start with a dot (.), like .help                     |\r\n");
 	socket.write("+-----------------------------------------------------------------------------+\r\n");
