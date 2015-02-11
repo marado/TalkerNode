d5625a3512700bb483106fdcc7177438e1e88d16
Neil Burrows <neil.burrows@nvable.com>
2015-02-11 11:36:53 +0000

    Build up output from help command automatically

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 exports.command = {
-	name: "quit", 			
+	name: "help", 			
 	autoload: true,			
 	unloadable: false,
 	min_rank: 0,
@@ -7,18 +7,31 @@ exports.command = {
 	help: "",
 
 	execute: function(socket, command, command_access) {
+
+
+		var userRank = command_access.getOnlineUser(socket.username).db.rank;
+
 	    socket.write("+-----------------------------------------------------------------------------+\r\n");
 	    socket.write("   Helpful commands on " + command_access.talkername + "\r\n");
 	    socket.write("+-----------------------------------------------------------------------------+\r\n");
-	    socket.write("| .help     - shows you this list of commands and what do they do             |\r\n");
-	    socket.write("| .say      - lets you talk with other people. Just .say something!           |\r\n");
-	    socket.write("| .emote    - lets you pose something, as if you were acting.                 |\r\n");
-	    socket.write("| .tell     - tells someone something, in private. Only both of you will know |\r\n");
-	    socket.write("| .who      - lets you know who is connected in the talker at this moment     |\r\n");
-	    socket.write("| .ranks    - list the talker's ranks, and points out which one is yourse     |\r\n");
-	    socket.write("| .version  - gives you information regarding the software this talker runs   |\r\n");
-	    socket.write("| .password - use this if you want to change your password                    |\r\n");
-	    socket.write("| .quit     - leaving us, are you? Then .quit !                               |\r\n");
+
+	    for(var c in command_access.commands) {
+	    	// Only show commands user has access to
+	    	if(userRank >= command_access.commands[c].min_rank ) {
+		    	var cmd = command_access.commands[c].name;
+		    	var desc = command_access.commands[c].display;
+
+		    	if(cmd.length > 10) {
+		    		cmd = cmd.substr(0, 10);
+		    	}
+
+		    	if(desc.length > 60) {
+		    		desc = desc.substr(0, 57) + "...";
+		    	}
+
+		    	socket.write("| ." +  cmd + Array(11 - cmd.length).join(' ') + " - " + desc + Array(62 - desc.length).join(' ') + " |\r\n");
+		    } 
+	    }
 	    socket.write("+-----------------------------------------------------------------------------+\r\n");
 	    socket.write("| Remember: all commands start with a dot (.), like .help                     |\r\n");
 	    socket.write("+-----------------------------------------------------------------------------+\r\n");
