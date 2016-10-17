956d4ef80c0baa1a3160782e0309b0740515fd8f
Marcos Marado <mindboosternoori@gmail.com>
2016-10-18 00:46:12 +0100

    .look now tells you who else is there with you
    Closes: #31

--- commands/look.js
+++ commands/look.js
@@ -3,15 +3,44 @@ exports.command = {
 	autoload: true,				// Should the command be autoloaded at startup
 	unloadable: true,			// Can the command be unloaded dynamically
 	min_rank: 0,				// Minimum rank to use to execute the command
-	display: "You look.",		// Summary help text to show in the .help command (Max 60 chars)
-	help: "You figure out where you are and where to can you go",			// Full help text when .help <command> is used
+	display: "You look.",
+	help: "You figure out where you are and where to can you go.",
 
 	// Function to execute the command
 	execute: function(socket, command, command_access) {
 		socket.write("You look around...\r\n");
-		socket.write("You notice you are at " + command_access.getUniverse().get(socket.db.where).name + ".\r\n");
+		socket.write("You notice you are at " +
+			command_access.getUniverse().get(socket.db.where).name + ".\r\n");
+		// look for other people
+		var others = [];
+		for (var i = 0; i < command_access.sockets.length; i++) {
+			if (
+				command_access.sockets[i].loggedin &&
+				command_access.sockets[i].db.where.toString() ==
+					socket.db.where.toString() &&
+				command_access.sockets[i].username !== socket.username
+			) {
+				others.push(command_access.sockets[i].username);
+			}
+		}
+		if (others.length > 0) {
+			var peeps = "You see " + others[0];
+			if (others.length === 1) {
+				peeps += ".";
+			} else {
+				for (var p = 1; p < others.length-1; p++) {
+					peeps += ", " + others[p];
+				}
+				peeps += " and " + others[others.length-1] + ".";
+			}
+			socket.write(peeps + "\r\n");
+		} else {
+			socket.write("You are alone here.\r\n");
+		}
 
-		var neighbours = command_access.getUniverse().get_neighbours(command_access.getUniverse().get(socket.db.where));
+		// look for exits
+		var neighbours = command_access.getUniverse().get_neighbours(
+			command_access.getUniverse().get(socket.db.where));
 		if (neighbours.length == 0) {
 			socket.write("You don't see anywhere to go to.\r\n");
 		} else {
