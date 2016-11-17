8e99abfb81214ab8e9296e9f6aea84921f3ee9b8
Marcos Marado <mindboosternoori@gmail.com>
2016-11-17 21:59:08 +0000

    Add stub implementation of .setcmdlev
    While this only lays the groundwork, and is not useful yet, having
    the command might be useful to fix a non-related bug ((#59).

---
+++ commands/setcmdlev.js
@@ -0,0 +1,40 @@
+exports.command = {
+	name: "setcmdlev",
+	autoload: false,
+	unloadable: false,
+	min_rank: 10,
+	display: "Changes the minimum rank necessary to use a particular command.",
+	help: "This command is used to change the minimum rank an user must have to be able to " +
+		"use a particular command.",
+	usage: "." + this.name + " <command> <rank number>",
+
+	execute: function(socket, command, command_access) {
+		if (command.split(' ').length !== 2) {
+			socket.write("Usage   : .setcmdlev <command> <rank number>\r\n");
+			return;
+		}
+		var c = command.split(' ')[0];
+		var r = command.split(' ')[1];
+		var command_to_change = command_access.commands[c];
+		if (!command_to_change) {
+			socket.write("Sorry, there is no command called " + c + ".\r\n");
+			return;
+		}
+		var rank_number = parseInt(r,10);
+		if (Number(r) !== rank_number || rank_number < 0) {
+			socket.write(r + " is no rank number.\r\n");
+			return;
+		}
+		if (rank_number === command_access.getCmdRank(c)) {
+			socket.write("Command " + c + " is already of rank level " +
+				rank_number + "!\r\n");
+			return;
+		}
+		if (rank_number >= command_access.ranks.list.length) {
+			socket.write("There aren't those many ranks!\r\n");
+			return;
+		}
+		socket.write("TODO: we should change " + c + " to rank " + rank_number +
+				".\r\n");
+	}
+}
