3390536a74af26bb74322721d8fc7b415649935d
Marcos Marado <mindboosternoori@gmail.com>
2016-11-27 03:28:25 +0000

    0.3.7: new .addrank command
    Ref.: #25

---
+++ commands/addrank.js
@@ -0,0 +1,40 @@
+exports.command = {
+	name: "addrank",
+	autoload: true,
+	unloadable: true,
+	min_rank: 9,
+	display: "adds a rank",
+	help: "Adds a rank.",
+	usage: ".addrank <rank name>",
+	weigth: 0,
+
+	// Function to execute the command
+	execute: function(socket, command, command_access) {
+		if (typeof command !== 'string' || command.length < 1) {
+			socket.write("You'll have to give the new rank a name...\r\n");
+			return;
+		}
+		// add the new rank
+		var updated = command_access.ranks;
+		updated.list.push(command);
+		command_access.updateRanks(updated);
+		// promote top-rank users to the new rank
+		var oldrank = updated.list.length - 2;
+		var users = command_access.getUsersList();
+		for (var u=0; u < users.length; u++) {
+			if (users[u].rank === oldrank) {
+				// promote users[u].username
+				var online = command_access.getOnlineUser(users[u].username);
+				if (online) {
+					online.db.rank = online.db.rank+1;
+					command_access.updateUser(online.username, online.db);
+				} else {
+					var w = command_access.getUser(users[u].username);
+					w.rank = w.rank+1;
+					command_access.updateUser(users[u].username, w);
+				}
+			}
+		}
+		socket.write("Rank added!\r\n");
+	}
+}
--- commands/ranks.js
+++ commands/ranks.js
@@ -9,12 +9,12 @@ exports.command = {
 
 	execute: function(socket, command, command_access) {
         var ranks = command_access.ranks;
-		socket.write("+-----------------------------------------------------------------------------+\r\n");
+		socket.write("+----------------------------------------------------------------------------+\r\n");
 		for (var r = 0 ; r < ranks.list.length; r++) {
 			var text = ("  " + r).slice(-3) + "\t: " + (ranks.list[r] + "            ").substr(0,11);
 			if (socket.db.rank == r) text += "\t<- you";
 			socket.write(text+"\r\n");
 		}
-		socket.write("+-----------------------------------------------------------------------------+\r\n");
+		socket.write("+----------------------------------------------------------------------------+\r\n");
 	}
 }
