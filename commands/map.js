7516ec01dfff4a9aaf29e614ff9b7ee9dbaaf163
Marcos Marado <mindboosternoori@gmail.com>
2016-12-21 22:25:46 +0000

    0.4.1: .map is now centered on the users' location
    Closes: #56

--- commands/map.js
+++ commands/map.js
@@ -8,6 +8,6 @@ exports.command = {
 	usage: ".map",
 
 	execute: function(socket, command, command_access) {
-		socket.write(command_access.getUniverse().asciimap());
+		socket.write(command_access.getUniverse().asciimap(socket.db.where));
 	}
 }
