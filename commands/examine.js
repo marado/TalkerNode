45deb785f5061625efa36974a755be9a06ba2257
Marcos Marado <mindboosternoori@gmail.com>
2016-12-28 22:13:19 +0000

    0.4.5: allow username abbreviations in all cases
    .exa now also abbreviates usernames.
    
    Closes: #53

--- commands/examine.js
+++ commands/examine.js
@@ -3,10 +3,11 @@ exports.command = {
 	autoload: true,				// Should the command be autoloaded at startup
 	unloadable: true,			// Can the command be unloaded dynamically
 	min_rank: 0,				// Minimum rank to use to execute the command
-	display: "Shows information about a user",		// Summary help text to show in the .help command (Max 60 chars)
+	display: "Shows information about a user", // Summary help text to show in the .help command
 	help: "Shows information about a user.\r\n" + 
 		"Without arguments, it will give you information about yourself.\r\n" + 
-		"If you add another user as an argument, it will show you info about him/her instead.",			// Full help text when .help <command> is used
+		"If you add another user as an argument, it will show you info about him/her " +
+		"instead.",
 	usage: ".examine <user>",
 
 	friendlyTime: function(ms) {
@@ -34,14 +35,28 @@ exports.command = {
 		if (typeof whom === 'undefined' || whom.length < 1) {
 			whom = socket.username;
 		}
+		var wArr = command_access.getAproxUser(whom);
+		if (wArr.length === 0) return socket.write(":: There's no one called " + whom + ".\r\n");
+		if (wArr.length > 1) {
+			var possibilities = "";
+			for (var p = 0; p < wArr.length - 1; p++) {
+				possibilities += wArr[p] + ", ";
+			}
+			possibilities += wArr[wArr.length - 1];
+			return socket.write(
+				"Be more explicit: whom do you want to examine (" +
+					possibilities + ")?\r\n"
+			);
+		}
+		whom = wArr[0];
 		w = command_access.getUser(whom);
-		whom = whom.toLowerCase().charAt(0).toUpperCase() + whom.toLowerCase().slice(1);
-		if (!w) return socket.write(":: There's no one called " + whom + ".\r\n");
 		socket.write(":: You examine " + whom + " and you see... \r\n");
 		if (typeof (w.registerTime) === 'undefined') {
 			socket.write(":: " + whom + " was registered in an old version.\r\n");
 		} else {
-			socket.write(":: " + whom + " was registered at " + new Date(w.registerTime).toString() + ".\r\n");
+			socket.write(
+				":: " + whom + " was registered at " + new Date(w.registerTime).toString() + ".\r\n"
+			);
 		}
 		if (typeof (w.totalTime) === 'undefined') {
 			// it either is his/her first time online, or it's an old user that
@@ -52,8 +67,13 @@ exports.command = {
 				socket.write(":: " + whom + " hasn't been online for a long time.\r\n");
 			}
 		} else {
-			socket.write(":: " + whom + " has been online for " + this.friendlyTime(w.totalTime) + ".\r\n");
+			socket.write(
+				":: " + whom + " has been online for " + this.friendlyTime(w.totalTime) + ".\r\n"
+			);
 		}
-		socket.write(":: " + whom + " is of rank " + command_access.ranks.list[w.rank] + ", and was last seen at " + command_access.getUniverse().get(w.where).name + ".\r\n");
+		socket.write(
+			":: " + whom + " is of rank " + command_access.ranks.list[w.rank] +
+				", and was last seen at " + command_access.getUniverse().get(w.where).name + ".\r\n"
+		);
 	}
 }
