528a032a48c5354db9708a16eb177fdfed61bc0e
Marcos Marado <mindboosternoori@gmail.com>
2017-10-29 01:29:43 +0100

    fix identation mess
    The file had a mix of tabs and spaces, now it's all tabs

--- commands/promote.js
+++ commands/promote.js
@@ -9,45 +9,45 @@ exports.command = {
 
 	// Function to execute the command
 	execute: function(socket, command, command_access) {
-        var me = socket;
+		var me = socket;
 		var whom = command.split(' ')[0];
-        var w = null;
-        // check if we got an whom
-        if (typeof whom === 'undefined' || whom.length < 1) {
-            return me.write("Promote whom?\r\n");
-        } else { // check if it's an user
-            var wArr = command_access.getAproxUser(whom);
-            if (wArr.length === 0) return me.write("Promote whom?\r\n");
-            if (wArr.length > 1) {
-                var possibilities = "";
-                for (var p = 0; p < wArr.length - 1; p++) {
-		    possibilities += wArr[p] + ", ";
+		var w = null;
+		// check if we got an whom
+		if (typeof whom === 'undefined' || whom.length < 1) {
+			return me.write("Promote whom?\r\n");
+		} else { // check if it's an user
+			var wArr = command_access.getAproxUser(whom);
+			if (wArr.length === 0) return me.write("Promote whom?\r\n");
+			if (wArr.length > 1) {
+				var possibilities = "";
+				for (var p = 0; p < wArr.length - 1; p++) {
+					possibilities += wArr[p] + ", ";
+				}
+				possibilities += wArr[wArr.length - 1];
+				return me.write("Be more explicit: whom do you want to promote ("+possibilities+")?\r\n");
+			}
 		}
-                possibilities += wArr[wArr.length - 1];
-                return me.write("Be more explicit: whom do you want to promote ("+possibilities+")?\r\n");
-            }
-        }
-        whom = wArr[0];
-        w = command_access.getUser(whom);
-        if (me.db.rank > w.rank) {
-            // if user is online, do it via sockets
-            var online = command_access.getOnlineUser(whom);
-            var rankName;
-            if (online) {
-                online.db.rank = online.db.rank+1;
-                rankName = command_access.ranks.list[online.db.rank];
-                command_access.updateUser(online.username, online.db);
-            } else {
-                w.rank = w.rank+1;
-                rankName = command_access.ranks.list[w.rank];
-                command_access.updateUser(whom, w);
-            }
-            whom = whom.toLowerCase().charAt(0).toUpperCase() + whom.toLowerCase().slice(1);
-            var sentence = ":: " + me.username + " has promoted " + whom + " to the rank of " + rankName + "! ::\r\n";
-            command_access.allButMe(socket,function(me,to){to.write(sentence);});
-            socket.write("You promoted " + whom + " to the rank of " + rankName + "!\r\n");
-        } else {
-            me.write("You cannot promote someone to an higher level than yourse!\r\n");
-        }
-    }
+		whom = wArr[0];
+		w = command_access.getUser(whom);
+		if (me.db.rank > w.rank) {
+			// if user is online, do it via sockets
+			var online = command_access.getOnlineUser(whom);
+			var rankName;
+			if (online) {
+				online.db.rank = online.db.rank+1;
+				rankName = command_access.ranks.list[online.db.rank];
+				command_access.updateUser(online.username, online.db);
+			} else {
+				w.rank = w.rank+1;
+				rankName = command_access.ranks.list[w.rank];
+				command_access.updateUser(whom, w);
+			}
+			whom = whom.toLowerCase().charAt(0).toUpperCase() + whom.toLowerCase().slice(1);
+			var sentence = ":: " + me.username + " has promoted " + whom + " to the rank of " + rankName + "! ::\r\n";
+			command_access.allButMe(socket,function(me,to){to.write(sentence);});
+			socket.write("You promoted " + whom + " to the rank of " + rankName + "!\r\n");
+		} else {
+			me.write("You cannot promote someone to an higher level than yourse!\r\n");
+		}
+	}
 }
