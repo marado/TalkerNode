7e0eb39e00b59a0f6cae855f34c8cb53e07d6bb3
Dissidente <dissidente@gmail.com>
2020-04-20 17:26:06 +0100

    CHANGE: Allow list size to be passed to .last as a parameter. When passing a numeric argument it will be assumed it is a list size setting, as usernames can't be numeric.

--- commands/last.js
+++ commands/last.js
@@ -6,9 +6,9 @@ exports.command = {
 	display: "Shows information about users last logins", // Summary help text to show in the .help command
 	help: "Shows information about users last logins.\r\n" + 
 		"Without arguments, it will give you information about who last logged in.\r\n" + 
-		"If you an user as an argument, it will show you info about his last login " +
-		"instead.",
-	usage: ".last [<user>]",
+		"If you specify an user as an argument, it will show you info about his " +
+		"last login instead. You can also specify how many records to show.",
+	usage: ".last [<user>|<list_size>]",
 
 	friendlyTime: function(ms) {
 		if (ms < 1000) {
@@ -32,49 +32,59 @@ exports.command = {
 	// Function to execute the command
 	execute: function(socket, command, command_access) {
 		var chalk = require('chalk');
-		var listSize = 15;	// size of the list of last users logging in
-		var whom = command.split(' ')[0];
+		if (parseInt(command.split(' ')[0]) || command.split(' ')[0] === "0") {
+			// command is a number; listSize setting
+			var listSize = Math.abs(parseInt(command.split(' ')[0]))+1;
+		} else {
+			// command is a username; whom setting
+			var listSize = 15;	// size of the list of last users logging in
+			var whom = command.split(' ')[0];
+		}
 		if (typeof whom === 'undefined' || whom.length < 1) {
 			// fetch list of last listSize logins:
-
-			// get all the users
-			var users = command_access.getUsersList();
-			// remove users without loginTime
-			var cleanlist = [];
-			for (var i = 0; i < users.length; i++) {
-				if (typeof users[i].loginTime !== 'undefined') {
-					cleanlist.push(users[i]);
+			if(listSize > 1)
+			{
+				// get all the users
+				var users = command_access.getUsersList();
+				// remove users without loginTime
+				var cleanlist = [];
+				for (var i = 0; i < users.length; i++) {
+					if (typeof users[i].loginTime !== 'undefined') {
+						cleanlist.push(users[i]);
+					}
 				}
-			}
-			// sort the list
-			// ...start by sorting the times
-			var times = [];
-			for (var i = 0; i < cleanlist.length; i++) {
-				times.push(cleanlist[i].loginTime);
-			}
-			times.sort(function(a, b){ return b-a; });
-			// ...trunk the results since we only care about showing listSize results
-			var sliced = times.slice(0,listSize-1);
-			// ...and then find the users for each of those times:
-			if (sliced.length === 0) {
-				command_access.sendData(socket, "There's nothing to let you know about, yet.\r\n");
-			} else {
-	   	 		command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
-	   	 		command_access.sendData(socket, chalk.cyan("+ " + chalk.bold("These are the last users to have logged in:") + "                                +\r\n"));
-	   	 		command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
-	   	 		for (var i = 0; i < sliced.length; i++) {
-					for (var j = 0; j < cleanlist.length; j++) {
-						if (cleanlist[j].loginTime === sliced[i]) {
-							command_access.sendData(socket, "  " + chalk.yellow(cleanlist[j].username) + 
-								" logged in at " + chalk.bold(new Date(cleanlist[j].loginTime).toString()) + ".  ");
-							if (command_access.getOnlineUser(cleanlist[j].username)) {
-								command_access.sendData(socket, chalk.yellow.bold("ONLINE"));
+				// sort the list
+				// ...start by sorting the times
+				var times = [];
+				for (var i = 0; i < cleanlist.length; i++) {
+					times.push(cleanlist[i].loginTime);
+				}
+				times.sort(function(a, b){ return b-a; });
+				// ...trunk the results since we only care about showing listSize results
+				var sliced = times.slice(0,listSize-1);
+				// ...and then find the users for each of those times:
+				if (sliced.length === 0) {
+					command_access.sendData(socket, "There's nothing to let you know about, yet.\r\n");
+				} else {
+					command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
+					command_access.sendData(socket, chalk.cyan("+ " + chalk.bold("These are the last users to have logged in:") + "                                +\r\n"));
+					command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
+					for (var i = 0; i < sliced.length; i++) {
+						for (var j = 0; j < cleanlist.length; j++) {
+							if (cleanlist[j].loginTime === sliced[i]) {
+								command_access.sendData(socket, "  " + chalk.yellow(cleanlist[j].username) + 
+									" logged in at " + chalk.bold(new Date(cleanlist[j].loginTime).toString()) + ".  ");
+								if (command_access.getOnlineUser(cleanlist[j].username)) {
+									command_access.sendData(socket, chalk.yellow.bold("ONLINE"));
+								}
+								command_access.sendData(socket, "\r\n");
 							}
-							command_access.sendData(socket, "\r\n");
 						}
 					}
-	   	 		}
-	   	 		command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
+					command_access.sendData(socket, chalk.cyan("+----------------------------------------------------------------------------+\r\n"));
+				}
+			} else {
+				command_access.sendData(socket, chalk.yellow + ":: So, you really don't want to know it, right?\r\n");
 			}
 		} else {
 			var wArr = command_access.getAproxUser(whom);
