48994ff79625fbc217622fa276913ab8b6e39b0b
Dissidente <dissidente@gmail.com>
2020-04-20 17:32:18 +0100

    Added .reloadcmds command
    Added the .reloadcmds command, to reload commands from within the
    talker session.
    
    Closes: #45
    Closes: #97

---
+++ commands/rcmds.js
@@ -0,0 +1,20 @@
+exports.command = {
+	name: "rcmds",
+	autoload: true,
+	unloadable: false,
+	min_rank: 10,
+	display: "reloads commands",
+	help: "Reloads commands set for autoload",
+	usage: ".rcmds",
+
+	execute: function(socket, command, command_access) {
+		var chalk = require('chalk');
+		try {
+			command_access.loadCommands();
+			command_access.sendData(socket, chalk.green(":: ") + "Commands reloaded\r\n");
+		} catch(err) {
+			command_access.sendData(socket,
+				chalk.red(":: ") + "Error executing command : " + err + "\r\n");
+		}
+	}
+}
