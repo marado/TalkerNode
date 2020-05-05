7739986cdd6d85bb9b39d12e8bef4c952495cb46
Dissidente <dissidente@gmail.com>
2020-05-05 18:04:18 +0100

    Housekeeping background loop, with .save
    * New saveTotalTime() function which updates total time counter for
      one or all users
    * New .save command to update total time counter for all users
    * New doHousekeeping() function with watchdog to perform periodical
      housekeeping (at the moment only saveTotalTime())
    
    For now, the housekeeping cycle runs for a period of time defined on
    the database, but we still don't have a command to configure that
    value.
    
    Closes: #90, #107

---
+++ commands/save.js
@@ -0,0 +1,19 @@
+exports.command = {
+	name: "save",
+	autoload: true,
+	unloadable: false,
+	min_rank: 7,
+	display: "Saves total time for all users",
+	help: "Saves total time for all users",
+	usage: ".save",
+	weigth: 0,
+
+	// Function to execute the command
+	execute: function(socket, command, command_access) {
+		var chalk = require('chalk');
+		command_access.saveTotalTime();
+		command_access.sendData(socket,
+			chalk.green(":: Total time for all users has been saved.\r\n")
+		);
+	}
+}
