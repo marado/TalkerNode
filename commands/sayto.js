253acbe68b2742c34cc9e25d2518a5c9aff81546
Marcos Marado <mindboosternoori@gmail.com>
2021-10-05 14:26:45 +0100

    0.6.35: improvements to .sayto
    * The text output to .sayto was changed slightly to enhance
      readability;
    * If .sayto is targetting the shortened name that might belong to more
      than one person, exclude the user's own name from the list of
      possibilities (it isn't probably they wanted to be talking to
      themselves...);
    * Now we check if two users are in the same room by checking its
      coordinates, not the name (two different rooms might have the same
      name!);
    * If we have a name's exact match, then we should not be looking into
      aproximates... If there is one user called "Mind" and another called
      "MindBooster", you should be able to "-mind hello" and be talking to
      "Mind" instead of questioned if you want that user or "MindBooster"
      instead.

--- commands/sayto.js
+++ commands/sayto.js
@@ -2,7 +2,7 @@ exports.command = {
 	name: "sayto",
 	aka: "to",
 	alias: "-",
-	autoload: true,			
+	autoload: true,
 	unloadable: false,
 	min_rank: 0,
 	display: "say something out loud directed at someone",
@@ -17,20 +17,33 @@ exports.command = {
 		if ((typeof to === 'undefined') || (typeof message === 'undefined') || to.length < 1 || message.length < 1) {
 			return command_access.sendData(socket, chalk.yellow(":: ") + "You have to use it this way:" + chalk.yellow(" .sayto <user> <text>\r\n"));
 		}
-
 		let possibleUsers = [];
-		for (let i = 0; i < command_access.sockets.length; i++) {
-			if (
-				command_access.sockets[i].loggedin
-				&& command_access.sockets[i].db.where.toString() === socket.db.where.toString()
-			) {
+		// there is a command_access.getAproxOnlineUser , but we're not just
+		// seeing who's online, we're also seein who's on the same room. It
+		// might make sense to turn this into an helper function in the future.
+		if (command_access.getOnlineUser(to) !== false && JSON.stringify(command_access.getOnlineUser(to).db.where) === JSON.stringify(socket.db.where)) {
+			// exact match
+			possibleUsers = [command_access.getOnlineUser(to)];
+		} else {
+			// looking for aproximate matches
+			for (let i = 0; i < command_access.sockets.length; i++) {
 				if (
-					to.toLowerCase() === command_access.sockets[i].username.toLowerCase().substr(0, to.length)
-					&& (to.length <= command_access.sockets[i].username.length)
+					command_access.sockets[i].loggedin
+					&& JSON.stringify(command_access.sockets[i].db.where) === JSON.stringify(socket.db.where)
 				) {
-					possibleUsers.push(command_access.sockets[i]);
+					if (
+						to.toLowerCase() === command_access.sockets[i].username.toLowerCase().substr(0, to.length)
+						&& (to.length <= command_access.sockets[i].username.length)
+					) {
+						possibleUsers.push(command_access.sockets[i]);
+					}
 				}
 			}
+		}
+
+		// if there are various possibilities, let's exclude the one where the user might be trying to talk to themselves
+		if (possibleUsers.length > 1) {
+			possibleUsers = possibleUsers.filter(user => user.username.toLowerCase() !== socket.username.toLowerCase());
 		}
 
 		if (!possibleUsers.length) {
@@ -40,9 +53,9 @@ exports.command = {
 				return command_access.sendData(socket, chalk.red(":: ") + "Talking to yourself is the first sign of madness.\r\n");
 			}
 			command_access.allHereButMe(socket, function(me,to){
-				to.write(`(${possibleUsers[0].username}) ${chalk.bold(me.username + ' says')} ${message}\r\n`);
+				to.write(`${chalk.bold(me.username + ' says')} (to ${possibleUsers[0].username}): ${message}\r\n`);
 			});
-			command_access.sendData(socket, `(${possibleUsers[0].username}) ${chalk.green('You say:')} ${message}\r\n`);
+			command_access.sendData(socket, `${chalk.green('You say')} (to ${possibleUsers[0].username}): ${message}\r\n`);
 		} else {
 			let possibilities = "";
 			for (let p = 0; p < possibleUsers.length - 1; p++) {
