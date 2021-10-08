c863a14fd2fd3fd9a412a8429ce0daf010aa9df6
Andrew Collington <andy@amnuts.com>
2021-10-08 23:35:30 +0100

    Experimenting with implementing NUTS-style colour formatting codes
    * Support for the NUTS-format codes such as ~FG. ~BK, ~OL, etc.
    * Support for 256-color output for terminals that support it with ~F152, ~B0, ~F15, etc.
    * Added to the `color` command to include `.color list` which outputs the color codes, in color if you have that on or monotone if you do not

--- commands/color.js
+++ commands/color.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 exports.command = {
 	name: "color",
 	aka: "colour",
@@ -6,18 +7,57 @@ exports.command = {
 	min_rank: 1,
 	display: "toggle colors on or off, for you",
 	help: "Toggles colors on or off, for you",
-	usage: ".color",
+	usage: [".color", ".color list"],
 
 
 	execute: function(socket, command, command_access) {
 		var chalk = require('chalk');
-		if (typeof socket.db.color === 'undefined') {
-			// By default color is turned on, so we're now turning it off:
-			socket.db.color = false;
-		} else {
-			socket.db.color = !(socket.db.color);
+		var list = command.split(' ')[0];
+
+		// set the colour if we don't have a list option
+		if (list === 'undefined' || list.length < 1 || list !== 'list') {
+			if (typeof socket.db.color === 'undefined') {
+				// By default color is turned on, so we're now turning it off:
+				socket.db.color = false;
+			} else {
+				socket.db.color = !(socket.db.color);
+			}
+			command_access.updateUser(socket.username, socket.db);
+			command_access.sendData(socket, chalk.green("[::] ") + "You have the color setting: " + socket.db.color + "\r\n");
+			return;
 		}
-		command_access.updateUser(socket.username, socket.db);
-		command_access.sendData(socket, chalk.green("[::] ") + "You have the color setting: " + socket.db.color + "\r\n");
+
+		const colorCodes = [
+			"~OL", "~UL", "~LI", "~RV",
+			"~FK", "~FR", "~FG", "~FY", "~FB", "~FM", "~FC", "~FW",
+			"~BK", "~BR", "~BG", "~BY", "~BB", "~BM", "~BC", "~BW",
+			"~RS"
+		];
+
+		colorCodes.forEach(code => {
+			command_access.sendData(socket, `^${code}: ${code}TalkerNode, version ${command_access.version}~RS\r\n`);
+		});
+
+		for (let i = 0; i < 256; i++) {
+			let code = '~F' + i;
+			let pad = i < 10 ? 2 : (i < 100 ? 1 : 0);
+			command_access.sendData(socket, code + ' ^' + code + `${' '.repeat(pad)} ~RS`);
+			if ((i+1) % 16 === 0) {
+				command_access.sendData(socket, "\r\n");
+			}
+		}
+		command_access.sendData(socket, "\r\n");
+
+		for (let i = 0; i < 256; i++) {
+			let code = '~B' + i;
+			let pad = i < 10 ? 2 : (i < 100 ? 1 : 0);
+			command_access.sendData(socket, code + ' ^' + code + `${' '.repeat(pad)} ~RS`);
+			if ((i+1) % 16 === 0) {
+				command_access.sendData(socket, "\r\n");
+			}
+		}
+		command_access.sendData(socket, "\r\n");
+
+
 	}
 }
--- utils/formatters.js
+++ utils/formatters.js
@@ -89,8 +89,78 @@ var friendly_time = function(ms) {
   return f_time;
 }
 
+let colorize = function(str) {
+  const colors = {
+    RS: "\033[0m", /* reset */
+    OL: "\033[1m", /* bold */
+    UL: "\033[4m", /* underline */
+    LI: "\033[5m", /* blink */
+    RV: "\033[7m", /* reverse */
+    /* Foreground colour */
+    FK: "\033[30m", /* black */
+    FR: "\033[31m", /* red */
+    FG: "\033[32m", /* green */
+    FY: "\033[33m", /* yellow */
+    FB: "\033[34m", /* blue */
+    FM: "\033[35m", /* magenta */
+    FC: "\033[36m", /* cyan */
+    FW: "\033[37m", /* white */
+    /* Background colour */
+    BK: "\033[40m", /* black */
+    BR: "\033[41m", /* red */
+    BG: "\033[42m", /* green */
+    BY: "\033[43m", /* yellow */
+    BB: "\033[44m", /* blue */
+    BM: "\033[45m", /* magenta */
+    BC: "\033[46m", /* cyan */
+    BW: "\033[47m", /* white */
+    /* Some compatibility names */
+    FT: "\033[36m", /* cyan AKA turquoise */
+    BT: "\033[46m", /* cyan AKA turquoise */
+  };
+
+  if (typeof str === 'object') {
+    return str;
+  }
+
+  str = str.replace(/(\^?~([A-Z]{2}))/g, (match, p1, p2, offset, inputString) => {
+    if (p1[0] === '^' || !colors.hasOwnProperty(p2)) {
+      return p1.substr(1);
+    }
+    return colors[p2];
+  });
+
+  str = str.replace(/(\^?~([FB])(\d{1,3}))/g, (match, p1, p2, p3, offset, inputString) => {
+    if (p1[0] === '^') {
+      return p1.substr(1);
+    }
+    let fb = p2 === 'F' ? 38 : 48;
+    return "\033[" + `${fb};5;${p3}m`;
+  });
+
+  return str;
+}
+
+let monotone = function(str) {
+  if (typeof str === 'object') {
+    return str;
+  }
+
+  str = str.replace(/(\^?~([A-Z]{2}))/g, (match, p1, p2, offset, inputString) => {
+    return (p1[0] === '^' ? p1.substr(1) : '');
+  });
+
+  str = str.replace(/(\^?~([FB])(\d{1,3}))/g, (match, p1, p2, p3, offset, inputString) => {
+    return (p1[0] === '^' ? p1.substr(1) : '');
+  });
+
+  return str;
+}
+
 module.exports = {
   text_wrap: text_wrap,
   line_wrap: line_wrap,
-  friendly_time: friendly_time
+  friendly_time: friendly_time,
+  colorize: colorize,
+  monotone: monotone
 }
